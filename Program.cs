9443dfad2f6e6e206fea72f6ca200819c48860e7
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-08-31 23:20:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PredictArrayOutput
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            String[] str = new String[] { "Sunflower", "Rose", "Jasmine", "Marigold", "Lily", "Lotus" };
+            String[] Info = str;
+            foreach (string k in Info)
+            {
+                Console.WriteLine(k[2]);
+            }
+
+        }
+    }
+
+//op:
+//n
+//s
+//s
+//r
+//l
+//t
+
+}
